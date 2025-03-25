842e17b1509b6ab4c1d05378c3b12f29d7cee5f4
Eugene Katrukha <katpyxa@gmail.com>
2025-03-25 23:54:15 +0100

    Bounds3D

---
+++ src/main/java/bvb/utils/Bounds3D.java
@@ -0,0 +1,73 @@
+package bvb.utils;
+
+public class Bounds3D
+{
+	private final double [] minBound;
+	private final double [] maxBound;
+
+	public Bounds3D( final double []  minBound, final double [] maxBound )
+	{
+		if(minBound.length != 3 || maxBound.length != 3)
+			throw new IllegalArgumentException();
+		for(int d=0; d<3; d++ )
+		{
+			if ( minBound[d] > maxBound[d] )
+				throw new IllegalArgumentException();
+		}
+		this.minBound = minBound;
+		this.maxBound = maxBound;
+	}
+	public double [] getMinBound()
+	{
+		return minBound;
+	}
+
+	public double [] getMaxBound()
+	{
+		return maxBound;
+	}
+	
+	public Bounds3D join( final Bounds3D other )
+	{
+		final double [] newMinBound = new double [3]; 
+		final double [] newMaxBound = new double [3];
+
+		for (int d=0; d<3; d++)
+		{
+			newMinBound[d] = Math.min( minBound[d], other.minBound[d] );
+			newMaxBound[d] = Math.max( maxBound[d], other.maxBound[d] );
+		}
+		
+		return new Bounds3D( newMinBound, newMaxBound );
+	}
+	
+	@Override
+	public String toString()
+	{
+		String output = "Bounds3D ";
+		for (int d=0; d<3; d++)
+		{
+			output = output + "[ "+ minBound[d] + ", " + maxBound[d] + " ]";
+		}
+		return output;
+	}
+	
+	@Override
+	public boolean equals( final Object o )
+	{
+		if ( this == o )
+			return true;
+		if ( o == null || getClass() != o.getClass() )
+			return false;
+
+		final Bounds3D that = ( Bounds3D ) o;
+		for (int d=0; d<3; d++)
+		{
+			if ( Double.compare( that.minBound[d], minBound[d] ) != 0 )
+				return false;
+			if ( Double.compare( that.maxBound[d], maxBound[d] ) != 0 )
+				return false;
+		}
+		return true;
+	}
+}
