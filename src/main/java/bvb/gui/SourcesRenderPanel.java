c597d1e6158e98fbbe8a071db1a319daee669295
Eugene Katrukha <katpyxa@gmail.com>
2025-04-04 23:35:30 +0200

    toggle boxes instead of combobox in render panel

--- src/main/java/bvb/gui/SourcesRenderPanel.java
+++ src/main/java/bvb/gui/SourcesRenderPanel.java
@@ -1,15 +1,19 @@
 package bvb.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.List;
 
-import javax.swing.JComboBox;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.UIManager;
 
 import bdv.tools.brightness.ConverterSetup;
@@ -23,9 +27,17 @@ public class SourcesRenderPanel extends JPanel implements ActionListener
 	
 	final SelectedSources selectedSources;
 	
-	public JComboBox<String> cbRenderMethod;
+	ButtonGroup renderMode = new ButtonGroup();
 	
-	public JComboBox<String> cbInterpolation; 
+	JToggleButton [] butRender = new JToggleButton[2];
+	
+	JPanel panRender;
+	
+	ButtonGroup interpolationMode = new ButtonGroup();
+	
+	JToggleButton [] butInter = new JToggleButton[2];
+	
+	JPanel panInterpolation;
 	
 	/**
 	 * Panel background if color reflects a set of sources all having the same color
@@ -39,10 +51,12 @@ public class SourcesRenderPanel extends JPanel implements ActionListener
 
 	public SourcesRenderPanel(final ConverterSetups convSetups, final SelectedSources selectedSources_)
 	{
-		super();
-		selectedSources = selectedSources_;
 		
-		setLayout(new GridBagLayout());
+		super(new GridBagLayout());
+		
+		int nButtonSize = 45;
+		
+		selectedSources = selectedSources_;
 		
 		this.setBorder(new PanelTitle(" Render "));
 
@@ -55,26 +69,57 @@ public class SourcesRenderPanel extends JPanel implements ActionListener
 		sInterpolation[1] = "Trilinear";
 		
 		GridBagConstraints gbc = new GridBagConstraints();
+	
+		URL icon_path = this.getClass().getResource("/icons/max_int.png");
+		ImageIcon tabIcon = new ImageIcon(icon_path);
+		butRender[0] = new JToggleButton(tabIcon);
+		butRender[0].setToolTipText("Maximum intensity");
+		
+		icon_path = this.getClass().getResource("/icons/volumetric.png");
+		tabIcon = new ImageIcon(icon_path);
+		butRender[1] = new JToggleButton(tabIcon);
+		butRender[1].setToolTipText("Volumetric");
+		
+		icon_path = this.getClass().getResource("/icons/nearest.png");
+		tabIcon = new ImageIcon(icon_path);
+		butInter[0] = new JToggleButton(tabIcon);
+		butInter[0].setToolTipText("Nearest neighbor");
+		
+		icon_path = this.getClass().getResource("/icons/linear.png");
+		tabIcon = new ImageIcon(icon_path);
+		butInter[1] = new JToggleButton(tabIcon);
+		butInter[1].setToolTipText("Trilinear");
 
-		cbRenderMethod = new JComboBox<>(sMethods);
-		cbRenderMethod.addActionListener(this);
+		panRender = new JPanel(new GridBagLayout());		
+		panInterpolation = new JPanel(new GridBagLayout());
+		
+		for(int i = 0; i<2; i++)
+		{
+			
+			butRender[i].setPreferredSize(new Dimension(nButtonSize , nButtonSize ));
+			butRender[i].addActionListener( this );
+			renderMode.add( butRender[i] );
+			panRender.add( butRender[i] );
+			
+			butInter[i].setPreferredSize(new Dimension(nButtonSize , nButtonSize ));
+			butInter[i].addActionListener( this );
+			interpolationMode.add( butInter[i] );
+			panInterpolation.add( butInter[i] );
+		}
+		
 		
 		gbc.gridx=0;
 		gbc.gridy=0;
 		GBCHelper.alighLoose(gbc);
-		this.add(new JLabel("View:"),gbc);
+		this.add(new JLabel("Render:"),gbc);
 		gbc.gridx++;
-		this.add(cbRenderMethod,gbc);		
+		this.add(panRender,gbc);			
 		
-		cbInterpolation = new JComboBox<>(sInterpolation);
-		
-		cbInterpolation.addActionListener(this);
-		
-		gbc.gridx=0;
+		gbc.gridx = 0;
 		gbc.gridy++;
-		this.add(new JLabel("Interpolation:"),gbc);
+		this.add(new JLabel("Voxels:"),gbc);
 		gbc.gridx++;
-		this.add(cbInterpolation,gbc);	
+		this.add( panInterpolation, gbc );
 		
 	    selectedSources.addSourceSelectionListener(  new SelectedSources.Listener()
 		{			
@@ -123,29 +168,34 @@ public class SourcesRenderPanel extends JPanel implements ActionListener
 		
 		if(bRenderConsistent)
 		{
-			cbRenderMethod.setBackground( consistentBg );
-			cbRenderMethod.setSelectedIndex( nRenderM );
+			butRender[nInterpM].setSelected( true );
+			panRender.setBackground( consistentBg );
 		}
 		else
 		{
-			cbRenderMethod.setBackground( inConsistentBg );
+			panRender.setBackground( inConsistentBg );
+			renderMode.clearSelection();
 		}
 		
 		if(bInterpConsistent)
 		{
-			cbInterpolation.setBackground( consistentBg );
-			cbInterpolation.setSelectedIndex( nInterpM );
+			butInter[nInterpM].setSelected( true );
+			panInterpolation.setBackground( consistentBg );
 		}
 		else
 		{
-			cbInterpolation.setBackground( inConsistentBg );
+			panInterpolation.setBackground( inConsistentBg );
+			interpolationMode.clearSelection();
 		}
 	}
 	
 	void setChoicesEnabled(boolean bEnabled)
 	{
-		cbRenderMethod.setEnabled( bEnabled );
-		cbInterpolation.setEnabled( bEnabled );
+		for(int i=0; i<2; i++)
+		{
+			butInter[i].setEnabled( bEnabled );
+			butRender[i].setEnabled( bEnabled );
+		}
 	}
 
 	@Override
@@ -155,18 +205,24 @@ public class SourcesRenderPanel extends JPanel implements ActionListener
 		
 		if(csList== null || csList.isEmpty())
 			return;
-		
-		for ( final ConverterSetup cs: csList)
+		for(int i=0;i<2;i++)
 		{
-			if(arg0.getSource() == cbRenderMethod)
-			{			
-			  ((GammaConverterSetup)cs).setRenderType( cbRenderMethod.getSelectedIndex() );
+			if(arg0.getSource() == butInter[i])
+			{
+				for ( final ConverterSetup cs: csList)
+				{
+					((GammaConverterSetup)cs).setVoxelRenderInterpolation( i );
+				}
+				return;
 			}
-			if(arg0.getSource() == cbInterpolation)
-			{			
-			  ((GammaConverterSetup)cs).setVoxelRenderInterpolation( cbInterpolation.getSelectedIndex() );
+			if(arg0.getSource() == butRender[i])
+			{
+				for ( final ConverterSetup cs: csList)
+				{
+					((GammaConverterSetup)cs).setRenderType( i );
+				}
+				return;
 			}
-
 		}
 	}
 	
