7ccfb894480d3849fd3d5dfc8a31da763e38eff5
Eugene Katrukha <katpyxa@gmail.com>
2025-04-11 22:05:19 +0200

    load sources panel v0

---
+++ src/main/java/bvb/gui/TabPanelDataSources.java
@@ -0,0 +1,18 @@
+package bvb.gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
+import bvb.core.BigVolumeBrowser;
+
+public class TabPanelDataSources extends JPanel
+{
+	public TabPanelDataSources(final BigVolumeBrowser bvb, final SelectedSources selectedSources)
+	{
+		super(new GridBagLayout());	
+		
+	    GridBagConstraints gbc = new GridBagConstraints();
+	}
+}
