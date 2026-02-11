aafd5dd9ceee3d79105b859b7d37aa9d9b0c39d5
Eugene Katrukha <katpyxa@gmail.com>
2026-02-11 13:17:02 +0100

    update viewer transform setting, remove listener in the end

--- src/main/java/bvb/gui/SetTransformAfterResize.java
+++ src/main/java/bvb/gui/SetTransformAfterResize.java
@@ -3,6 +3,7 @@ package bvb.gui;
 import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import javax.swing.Timer;
 
@@ -10,21 +11,26 @@ import net.imglib2.realtransform.AffineTransform3D;
 
 import bvvpg.core.VolumeViewerPanel;
 
+/** wait for BVV window to finish window resizing 
+ * before applying viewer transform **/
 public class SetTransformAfterResize
 {
 
 	public static void run(final VolumeViewerPanel viewer, final AffineTransform3D viewTransform)
 	{
-		
-		Dimension lastSize = new Dimension();
+		ComponentListener[] holder = new ComponentListener[1];
+		final Dimension lastSize = new Dimension();
 				
 		Timer resizeTimer = new Timer(200, e -> {
 		    //System.out.println("Resize finished.");
+			viewer.removeComponentListener(holder[0]);
 		    viewer.state().setViewerTransform( viewTransform );
 		});
 		resizeTimer.setRepeats(false);
-
-		viewer.addComponentListener(new ComponentAdapter() {
+		lastSize.setSize( viewer.getSize());
+		
+		holder[0] = new ComponentAdapter() 
+		{
 		    @Override
 		    public void componentResized(ComponentEvent e) {
 		        Dimension current = viewer.getSize();
@@ -33,6 +39,8 @@ public class SetTransformAfterResize
 		            resizeTimer.restart();
 		        }
 		    }
-		});
+		};
+		
+		viewer.addComponentListener(holder[0]);
 	}
 }
