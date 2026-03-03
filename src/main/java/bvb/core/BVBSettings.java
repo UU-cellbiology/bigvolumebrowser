9393a6308470b8aa0c6be3b33037d15d16ee3f3e
Eugene Katrukha <katpyxa@gmail.com>
2026-03-03 22:03:57 +0100

    version bump

--- src/main/java/bvb/core/BVBSettings.java
+++ src/main/java/bvb/core/BVBSettings.java
@@ -35,7 +35,7 @@ import ij.Prefs;
 public class BVBSettings
 {
 	
-	public static String sVersion = "0.0.8";
+	public static String sVersion = "0.0.9";
 	
 	/** background color of BVV canvas **/
 	public static Color canvasBGColor = new Color((int)Prefs.get( "BVB.canvasBGColor", Color.BLACK.getRGB() ));
