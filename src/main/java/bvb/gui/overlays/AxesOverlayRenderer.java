a1055203b6a1caffb9df9e22e362597487a8d7b9
Eugene Katrukha <katpyxa@gmail.com>
2026-03-13 09:02:49 +0100

    cleanup axes gizmo overlay

--- src/main/java/bvb/gui/overlays/AxesOverlayRenderer.java
+++ src/main/java/bvb/gui/overlays/AxesOverlayRenderer.java
@@ -48,48 +48,59 @@ import bvb.core.BVBActions.AlignPlaneBVB;
 public class AxesOverlayRenderer implements OverlayRenderer
 {
 	private AbstractViewerPanel viewer = null;
-	
+
+	private volatile boolean isEnabled = false;
+
+	boolean isHover = false;
+
+	int nHighlightedAxis = -1;
+
 	final Color [] axesColors = new Color[] 
-//			{new Color(255,54,83,255), new Color(118,178,23,255), new Color(48,121,204,255)};
 			{new Color(255,54,83,255), new Color(118,178,23,255), new Color(48,121,204,255),
 			new Color(255,54,83,128), new Color(118,178,23,128), new Color(48,121,204,128)};
 
-	//final String [] axisTitle = new String [] {"X","Y","Z"};
-	
 	final Color hoverBGColor = new Color( 0xb0bbbbbb, true );
 	final BasicStroke vectorStroke = new BasicStroke(2.5f);
 	final BasicStroke ovalStroke = new BasicStroke(1);
 	final BasicStroke letterStroke = new BasicStroke(1.3f);
 	
-	private volatile boolean isEnabled = false;
-	
-	boolean isHover = false;
-	
 	final double [] qRotation = new double[4];
 	
+	/** center of the gizmo **/
 	final Point center = new Point();
 	
 	final int vectRadius = 40;
 	final int circleAxisRadius = 9;	
 	final int fullRadiusSquared = (vectRadius + circleAxisRadius) * (vectRadius + circleAxisRadius);
+	
+	/** current centers of circles with axes labels
+	 * 0,1,2 - positive direction
+	 * 3,4,5 - negative **/
 	final double [][] dAxisCircleCenter = new double [6][3];
+	
+	/** current ends of vector "sticks" from origin to the  circles with axis labels
+	 * 0,1,2 - positive direction
+	 * 3,4,5 - negative **/	
 	final double [][] dAxisVector = new double [6][3];
 
+	/** ordered indexes of circles/axes after sorting with respect to depth **/
 	final double [][] axisOrder = new double [6][2];
-
-	int nHighlightedAxis = -1;
 	
 	private final static int xLetterHalf = 3;
-	private final static int yLetterHalf = 4;
 	
+	private final static int yLetterHalf = 4;
+
+	/** set of stored quaternions corresponding to the aligned planes **/
 	private final double [][] quaterLibrary = new double[6][4];
 	
+	/** is equal to the axis index if the current orientation
+	 * aligns with one of the determined align planes or -1 otherwise **/
 	private int nAlignIndex;
 	
 	public AxesOverlayRenderer()
 	{
 		super();
-		//there should be an easier way?!
+		//current plane align quaternions
 		for(int d = 0; d < 4; d++)
 		{
 			LinAlgHelpers.quaternionInvert( AlignPlaneBVB.ZY.qAlign, quaterLibrary[0] );
@@ -114,10 +125,18 @@ public class AxesOverlayRenderer implements OverlayRenderer
 			return;
 
 		Graphics2D graphics = (Graphics2D) g;
+		
+		graphics.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+		
 		final Rectangle clipBounds = g.getClipBounds();
-		center.setLocation( clipBounds.getWidth() - 80 , 100 );
+		
+		center.setLocation( clipBounds.getWidth() - 80, 100 );
+		
 		isHover = false;
+		
 		final Point pMouse = viewer.getMousePosition();
+		
+		//draw a grey background circle if mouse is on top of the gizmo
 		if (pMouse != null)
 		{
 			final float dMouseX = pMouse.x - center.x;
@@ -133,12 +152,13 @@ public class AxesOverlayRenderer implements OverlayRenderer
 				isHover = true;
 			}
 		}
-
-		graphics.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+		
 		AffineTransform3D transform = new AffineTransform3D();
+		
 		viewer.state().getViewerTransform(transform);
 
 		Affine3DHelpers.extractRotationAnisotropic( transform, qRotation );
+		
 		for(int d = 0; d < 6; d++)
 		{
 			for(int i = 0; i < 3; i ++)
@@ -158,18 +178,24 @@ public class AxesOverlayRenderer implements OverlayRenderer
 			}
 			LinAlgHelpers.quaternionApply( qRotation, dAxisCircleCenter[d], dAxisCircleCenter[d] );
 			LinAlgHelpers.quaternionApply( qRotation, dAxisVector[d], dAxisVector[d] );
-			//z-coordinate
-			axisOrder[d][0] = dAxisCircleCenter[d][2]; 
-			//index
-			axisOrder[d][1] = d; 	
+
 			dAxisCircleCenter[d][0] += center.x;
 			dAxisCircleCenter[d][1] += center.y;
 			dAxisVector[d][0] += center.x;
 			dAxisVector[d][1] += center.y;
+
+			//prepare array for the sorting along depth 
+			//z-coordinate
+			axisOrder[d][0] = dAxisCircleCenter[d][2]; 
+			//index
+			axisOrder[d][1] = d; 			
 		}
 
-		//highlight axis if hover is active
-		if(isHover)
+		//sort axes circles positions by depth (z-coord) for rendering
+		Arrays.sort(axisOrder, (a, b) -> (-1) * Double.compare(a[0], b[0]));
+
+		//find highlight axis circle index that closest to the mouse pointer, if hover is active
+		if( isHover )
 		{
 			float dx, dy;
 			dx = (float)dAxisCircleCenter[0][0] - pMouse.x;
@@ -185,29 +211,31 @@ public class AxesOverlayRenderer implements OverlayRenderer
 					nHighlightedAxis = d;
 					fDistance = dx * dx + dy * dy;
 				}
-
 			}
 		}
 		else
 		{
 			nHighlightedAxis = -1;
 		}
-		//sort by depth (z-coord)
-		Arrays.sort(axisOrder, (a, b) -> (-1) * Double.compare(a[0], b[0]));
+		// see if the current orientation is aligned to predetermined library
 		nAlignIndex = isAlignedWithPlanes(qRotation);
+		
 		for( int d = 0; d < 6; d++)
 		{
 			graphics.setStroke(vectorStroke);
+			
 			final int index = (int)axisOrder[d][1];
 
 			int x = (int) Math.round( dAxisCircleCenter[index][0]);
 			int y = (int) Math.round( dAxisCircleCenter[index][1]);
+			
 			//positive axes
 			if(index < 3)
 			{
 				//vector's body
 				graphics.setColor(axesColors[index]);					 
 				graphics.drawLine(center.x, center.y, x, y);
+				//circle at the end of vector
 				graphics.setStroke(ovalStroke);
 				graphics.fillOval( x - circleAxisRadius, y - circleAxisRadius , 2 * circleAxisRadius + 2 , 2 * circleAxisRadius + 2);
 				drawLetter(graphics, x + 1 , y + 1, index);
@@ -216,15 +244,15 @@ public class AxesOverlayRenderer implements OverlayRenderer
 			else
 			{
 				graphics.setColor(axesColors[index].darker());
+				//if aligned, draw vectors, otherwise not
 				if(nAlignIndex >= 0)
 				{
 					graphics.drawLine(center.x, center.y, (int) Math.round( dAxisVector[index][0]), (int) Math.round( dAxisVector[index][1]));
-					//graphics.drawLine(center.x, center.y, x, y);
 
 					if(nAlignIndex == index)
 						graphics.setColor(axesColors[index - 3]);
 				}
-					
+				//draw transparent circles	
 				graphics.setStroke(ovalStroke);
 				graphics.fillOval( x - circleAxisRadius, y - circleAxisRadius , 2 * circleAxisRadius + 2 , 2 * circleAxisRadius + 2);
 				graphics.setColor(axesColors[index - 3]);
@@ -232,11 +260,9 @@ public class AxesOverlayRenderer implements OverlayRenderer
 				drawLetter(graphics, x + 1 , y + 1, index);
 			}
 		}
-
-
 	}
 	
-	/** since fonts vary from OS to OS, I will draw axes letter with lines.**/
+	/** since fonts appearance etc depends on OS, labels (letters) of axes are drawn with lines.**/
 	void drawLetter(final Graphics2D graphics, final int x, final int y, final int index)
 	{
 		if( nHighlightedAxis == index )
@@ -245,25 +271,25 @@ public class AxesOverlayRenderer implements OverlayRenderer
 			graphics.setPaint( Color.BLACK );
 		
 		graphics.setStroke(letterStroke);
-		int nShiftedX = x;
+		int nFinalX = x;
 		int nFinIndex = index;
 		if (index > 2)
 		{
 			if(nHighlightedAxis == index)
 			{
-				//draw minus
-				graphics.drawLine( nShiftedX -2*xLetterHalf, y, nShiftedX - xLetterHalf, y);
+				//draw minus sign
+				graphics.drawLine( nFinalX -2*xLetterHalf, y, nFinalX - xLetterHalf, y);
 
 				nFinIndex = index - 3;
-				nShiftedX += 3;
+				nFinalX += 3;
 			}
 			else if(nAlignIndex == index )
 			{
 				//draw minus
-				graphics.drawLine( nShiftedX -2*xLetterHalf, y, nShiftedX - xLetterHalf, y);
+				graphics.drawLine( nFinalX -2*xLetterHalf, y, nFinalX - xLetterHalf, y);
 
 				nFinIndex = index - 3;
-				nShiftedX += 3;
+				nFinalX += 3;
 				
 			}
 			else
@@ -275,20 +301,20 @@ public class AxesOverlayRenderer implements OverlayRenderer
 		{
 		//x
 		case 0:
-			graphics.drawLine( nShiftedX - xLetterHalf, y - yLetterHalf, nShiftedX + xLetterHalf, y + yLetterHalf);
-			graphics.drawLine( nShiftedX - xLetterHalf, y + yLetterHalf, nShiftedX + xLetterHalf, y - yLetterHalf);
+			graphics.drawLine( nFinalX - xLetterHalf, y - yLetterHalf, nFinalX + xLetterHalf, y + yLetterHalf);
+			graphics.drawLine( nFinalX - xLetterHalf, y + yLetterHalf, nFinalX + xLetterHalf, y - yLetterHalf);
 			break;
 		//y
 		case 1:
-			graphics.drawLine( nShiftedX - xLetterHalf, y - yLetterHalf, nShiftedX, y + 1 );
-			graphics.drawLine( nShiftedX + xLetterHalf, y - yLetterHalf, nShiftedX, y + 1);
-			graphics.drawLine( nShiftedX, y + 1,nShiftedX, y + yLetterHalf);
+			graphics.drawLine( nFinalX - xLetterHalf, y - yLetterHalf, nFinalX, y + 1 );
+			graphics.drawLine( nFinalX + xLetterHalf, y - yLetterHalf, nFinalX, y + 1);
+			graphics.drawLine( nFinalX, y + 1,nFinalX, y + yLetterHalf);
 			break;
 		//z
 		case 2:
-			graphics.drawLine( nShiftedX - xLetterHalf, y - yLetterHalf, nShiftedX + xLetterHalf, y - yLetterHalf);
-			graphics.drawLine( nShiftedX - xLetterHalf, y + yLetterHalf, nShiftedX + xLetterHalf, y - yLetterHalf);
-			graphics.drawLine( nShiftedX - xLetterHalf, y + yLetterHalf, nShiftedX + xLetterHalf, y + yLetterHalf);
+			graphics.drawLine( nFinalX - xLetterHalf, y - yLetterHalf, nFinalX + xLetterHalf, y - yLetterHalf);
+			graphics.drawLine( nFinalX - xLetterHalf, y + yLetterHalf, nFinalX + xLetterHalf, y - yLetterHalf);
+			graphics.drawLine( nFinalX - xLetterHalf, y + yLetterHalf, nFinalX + xLetterHalf, y + yLetterHalf);
 			break;
 
 		default:
